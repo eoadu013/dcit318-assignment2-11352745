829e1bf73ca384d0dc528d1e79e69592cf3d8af4
Eric Osei Adu <eoadu013@st.ug.edu.gh>
2025-08-04 23:53:32 +0000

    Implement basic inheritance structure with Animal, Dog, and Cat classes

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace InheritanceDemo
+{
+    // Base class
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    // Derived class Dog
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    // Derived class Cat
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal genericAnimal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            genericAnimal.MakeSound();
+            dog.MakeSound();
+            cat.MakeSound();
+        }
+    }
+}
