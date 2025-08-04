f1c45f6c3c5b268913221678957ee998deb852ed
Eric Osei Adu <eoadu013@st.ug.edu.gh>
2025-08-04 23:56:40 +0000

    Implement abstract class structure with Shape, Circle, and Rectangle classes

--- Abstraction/Program.cs
+++ Abstraction/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace AbstractClassDemo
+{
+    // Abstract class
+    abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape circle = new Circle(5);
+            Shape rectangle = new Rectangle(4, 6);
+
+            Console.WriteLine($"Circle Area: {circle.GetArea()}");
+            Console.WriteLine($"Rectangle Area: {rectangle.GetArea()}");
+        }
+    }
+}
